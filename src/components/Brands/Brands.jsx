9016d794d03b9b8a44f629b3c037f9f6604ef6f7
gauravKverma <gauravverma004@gmail.com>
2022-07-21 22:56:04 +0530

    day-2

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,29 @@
+import { Box, Heading, Image, Stack } from '@chakra-ui/react'
+import React from 'react'
+import { BrandsData } from './BrandsData'
+
+const Brands = () => {
+  return (
+    <div style={{margin:"auto", width:"95%"}}>
+        <Heading fontSize="26px" textAlign="left" marginTop="60px">
+        Featured Brands
+        </Heading>
+        <p style={{fontWeight:"500",color:"#6d757c",textAlign:"left",marginTop:"5px"}}>
+            Pick from our favourite brands
+        </p>
+        <Stack direction="horizontal" gap="30px"
+        justify="space-between" overflow="scroll" marginTop="25px">
+            {BrandsData.map((el,i) => (
+                <Box width="180px" height="212px">
+                    <img style={{borderRadius:"5px",width:"180px", height:"172px",cursor:"pointer"}} src={el.img}/>
+                    <Box width="180px" height="212px">
+                        <p style={{fontWeight:"500"}}>{el.heading}</p>
+                    </Box>
+                </Box>
+            ))}
+        </Stack>
+    </div>
+  )
+}
+
+export default Brands
